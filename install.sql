5695bab32ae45646c5e4d842697a44c291ec57cc
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-02-21 11:17:54 +0100

    Termine in core

--- install.sql
+++ install.sql
@@ -2,12 +2,5 @@ CREATE TABLE `statusgruppen_additional` (
   `statusgruppe_id` varchar(32) NOT NULL,
   `waitinglist` enum('true','false') NOT NULL DEFAULT 'false',
   `visible` enum('true','false') NOT NULL DEFAULT 'true',
-  `termine` enum('true','false') NOT NULL DEFAULT 'false',
   PRIMARY KEY (`statusgruppe_id`)
-);
-
-CREATE TABLE `statusgruppen_termine` (
-`statusgruppe_id` varchar(32) NOT NULL,
-`assign_id` varchar(32) NOT NULL,
-PRIMARY KEY (`statusgruppe_id`,`assign_id`)
 );
--- uninstall.sql
+++ uninstall.sql
@@ -1,3 +1,1 @@
 DROP TABLE `statusgruppen_additional`;
-
-DROP TABLE `statusgruppen_termine`;
